17e63bd7488dd9ed5deb87e66f47e04600f65590
Tom Heaton <thomas.m.heaton@gmail.com>
2024-06-25 17:11:43 +0100

    add more blocks

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ func main() {
 
 	blockchain.addBlock("A", "B", 7)
 	blockchain.addBlock("B", "C", 9)
+	blockchain.addBlock("C", "D", 3)
+	blockchain.addBlock("D", "E", 5)
+	blockchain.addBlock("E", "F", 2)
 
 	blockchain.viewBlockchain()
 	fmt.Printf("Blockchain valid: %v\n", blockchain.isValid())
